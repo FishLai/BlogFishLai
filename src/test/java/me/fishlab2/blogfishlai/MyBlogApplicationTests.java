3d4d2aa138d0268068a99839aeca60d21bbee023
FishLai <quan_yu_lai@outlook.com>
2021-02-23 11:36:17 +0800

    create new project
    start dev blog project

---
+++ src/test/java/me/fishlab2/blogfishlai/MyBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package me.fishlab2.blogfishlai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
